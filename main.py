8cc679c233e6b6b6d180bc1fa2f818e89424a882
VineMaionese <viniciushallmann2015@gmail.com>
2025-03-03 12:39:10 -0300

    update main to call cache

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ def main():
     p = Parser()
     args = p.parse()
 
+    cache = Cache(nsets=args.nsets, bsize=args.bsize,assoc=args.assoc, subst=args.subst, flagOut=args.flagOut, arquivoEntrada=args.arquivoEntrada)
+    cache.simulate()
+
 
 if __name__ == '__main__':
     main()
